62c9c4edd7d8d4e20533f4a5f37f3eb13ea0c03d
Ibai Pérez Fernández <ibaperfer@alum.us.es>
2025-11-10 19:35:42 +0100

    test:Tests del modulo car_check realizados.#1

--- app/modules/car_check/tests/locustfile.py
+++ app/modules/car_check/tests/locustfile.py
@@ -1,21 +1,50 @@
-from locust import HttpUser, TaskSet, task
+import os
+import random
+import sys
+
+from locust import HttpUser, between, task
 from core.environment.host import get_host_for_locust_testing
+from app import create_app
+from app.modules.hubfile.models import Hubfile
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../.."))
+sys.path.insert(0, project_root)
 
 
-class CarCheckBehavior(TaskSet):
-    def on_start(self):
-        self.index()
+def get_all_file_ids():
+    """
+    Obtiene todos los ids de la base de datos.
+    """
+    app = create_app()
+    
+    with app.app_context():
+        try:
+            ids = [hubfile.id for hubfile in Hubfile.query.with_entities(Hubfile.id).all()]
+            print(f"Obtenidos {len(ids)} IDs desde la base de datos para el test de carga.")
+            return ids
+        except Exception as e:
+            print(f"Error al conectar con la base de datos para obtener los IDs: {e}")
+            return []
+        
+        
+AVAILABLE_FILES = get_all_file_ids()
 
-    @task
-    def index(self):
-        response = self.client.get("/car_check")
 
-        if response.status_code != 200:
-            print(f"CarCheck index failed: {response.status_code}")
-
-
-class CarCheckUser(HttpUser):
-    tasks = [CarCheckBehavior]
-    min_wait = 5000
-    max_wait = 9000
+class CarCheckUsers(HttpUser):
+    """
+    Define el comportamiento del usuario durante la prueba de carga
+    """
+    
+    wait_time = between(4, 8)
     host = get_host_for_locust_testing()
+    
+    @task
+    def check_car_file_set(self):
+        """
+        Simula la petición de un archivo .car en concreto
+        Utiliza un ID aleatorio de los disponibles
+        """
+        
+        if AVAILABLE_FILES:
+            file_id = random.choice(AVAILABLE_FILES)
+            self.client.get(f"/car_check/{file_id}", name="car_check/[file_id]")
