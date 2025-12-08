922aed31a242df1deae1b202347323b23b525e22
Francisco Manuel Sabido Gonzalez <frasabgon@alum.us.es>
2025-12-08 21:25:01 +0100

    fix: posible arreglo migraciones [#17]

---
+++ migrations/versions/c833bcfbe8e6_merge_heads.py
@@ -0,0 +1,24 @@
+"""Merge heads
+
+Revision ID: c833bcfbe8e6
+Revises: 1a6c8e9ee0e6, 38a55f1d32bb
+Create Date: 2025-12-08 21:21:24.977642
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c833bcfbe8e6'
+down_revision = ('1a6c8e9ee0e6', '38a55f1d32bb')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
