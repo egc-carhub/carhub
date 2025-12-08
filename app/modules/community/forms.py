b465e274c0b81a966be8183943c46d9411dc11a8
Ibai Pérez Fernández <ibaperfer@alum.us.es>
2025-12-08 18:53:52 +0100

    feat: Community templates created

--- app/modules/community/forms.py
+++ app/modules/community/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 
 class CommunityForm(FlaskForm):
-    submit = SubmitField('Save community')
+    name = StringField('Community Name', validators=[DataRequired(), Length(max=50)])
+    description = TextAreaField('Description', validators=[Length(max=255)])
+    submit = SubmitField('Create Community')
