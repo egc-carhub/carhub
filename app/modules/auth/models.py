49b1cbe150b954a876c09e64b9b0a1ab71f1e65b
Ibai Pérez Fernández <ibaperfer@alum.us.es>
2025-12-08 18:53:52 +0100

    feat: Community templates created

--- app/modules/auth/models.py
+++ app/modules/auth/models.py
@@ -19,6 +19,11 @@ class User(db.Model, UserMixin):
 
     data_sets = db.relationship("DataSet", backref="user", lazy=True)
     profile = db.relationship("UserProfile", backref="user", uselist=False)
+    communities = db.relationship(
+        'Community',
+        secondary='community_members',
+        back_populates='community_members'
+    )
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
